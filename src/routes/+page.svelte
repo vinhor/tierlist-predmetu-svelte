1562f3051a0d68d297a5adecf1ea4e4fe013e500
vinhor <88082548+vinhor@users.noreply.github.com>
2024-05-13 22:28:10 +0200

    prep for gh pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import "./../lib/style.css";
+  import TableElement from "./TableElement.svelte";
 </script>
 
 <h1>Tierlist učitelů</h1>
 <table>
   <tr>
     <th scope="row">S</th>
+    <td></td>
   </tr>
   <tr>
     <th scope="row">A</th>
---
+++ src/routes/TableElement.svelte
@@ -0,0 +1,3 @@
+<script>
+    export let predmet;
+</script>
